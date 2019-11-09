cef6dab5967f9c87ca9245234c6507a7e7e94a71
Timur <55026550+SVizor42@users.noreply.github.com>
2019-11-09 10:35:39 +0300

    Assignment 2
    Initial commit

---
+++ assignment_2/3_2/main.cpp
@@ -0,0 +1,77 @@
+﻿/*
+3_1.Первые k элементов длинной последовательности
+
+Дана очень длинная последовательность целых чисел длины n. Требуется вывести в отсортированном виде её наименьшие k элементов.
+Последовательность может не помещаться в память.Время работы O(n* log(k)). Доп.память O(k). Использовать слияние.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+
+void Merge(int* leftArr, int leftLen, int* rightArr, int rightLen, int* buffer) {
+	int i = 0, j = 0;
+	while (i < leftLen && j < rightLen) {
+		if (leftArr[i] <= rightArr[j]) {
+			buffer[i + j] = leftArr[i];
+			i++;
+		}
+		else {
+			buffer[i + j] = rightArr[j];
+			j++;
+		}
+	}
+
+	if (i < leftLen) {
+		while (i < leftLen) {
+			buffer[i + j] = leftArr[i];
+			i++;
+		}
+	}
+	else {
+		while (j < rightLen) {
+			buffer[i + j] = rightArr[j];
+			j++;
+		}
+	}
+}
+
+void MergeSort(int* arr, int len) {
+	if (len <= 1) {
+		return;
+	}
+	int leftLen = len / 2;
+	int rightLen = len - leftLen;
+	MergeSort(arr, leftLen);
+	MergeSort(arr + leftLen, rightLen);
+
+	int* buffer = new int[len];
+	Merge(arr, leftLen, arr + leftLen, rightLen, buffer);
+	memcpy(arr, buffer, sizeof(int) * len);
+
+	delete[] buffer;
+}
+
+int main()
+{
+	int n = 0, k = 0;
+	std::cin >> n >> k;
+	
+	std::vector<int> numbers(n);
+	for (int i = 0; i < n; i++) {
+		std::cin >> numbers[i];
+	}
+
+	// Пробегаем сортировкой слиянием по исходному массиву с шагом k скользящим окном шириной в 2*k 
+	for (int i = 0; i < n; i += k) {
+		MergeSort(&numbers[i], std::min(2*k, n - i));
+	}
+
+	for (int j = 0; j < n; j++) {
+		std::cout << numbers[j] << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
---
+++ assignment_2/4_4/main.cpp
@@ -0,0 +1,71 @@
+﻿/*
+
+4. Порядковая статистика
+
+Даны неотрицательные целые числа n, k и массив целых чисел из диапазона[0..109] размера n.
+Требуется найти k - ю порядковую статистику.т.е.напечатать число, которое бы стояло на позиции с индексом k ∈[0..n - 1] в отсортированном массиве.
+Напишите нерекурсивный алгоритм.
+Требования к дополнительной памяти : O(n).
+Требуемое среднее время работы : O(n).
+Функцию Partition следует реализовывать методом прохода двумя итераторами в одном направлении.
+
+4_4.Реализуйте стратегию выбора опорного элемента “случайный элемент”.
+Функцию Partition реализуйте методом прохода двумя итераторами от конца массива к началу.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
+int getRandomPivot(int n) {
+	return std::rand() % n;
+}
+
+int Partition(int* arr, int len, int k) {
+	std::swap(arr[0], arr[k]);
+
+	int i = len - 1;
+	for (int j = i; j > 0; j--) {
+		if (arr[j] > arr[0] || (arr[j] == arr[0] && j % 2 == 0)) {
+			std::swap(arr[j], arr[i]);
+			i--;
+		}
+	}
+
+	std::swap(arr[0], arr[i]);
+	return i;
+}
+
+int getKStatistics(int* arr, int len, int k) {
+	int pivot = 0;
+	for (;;) {
+		pivot = getRandomPivot(len);
+		pivot = Partition(arr, len, k);
+		if (pivot == k) {
+			return arr[pivot];
+		}
+		if (pivot > k) {
+			len = pivot;
+		}
+		else {
+			arr += (pivot + 1);
+			len -= (pivot + 1);
+			k -= (pivot + 1);
+		}
+	}
+}
+
+int main()
+{
+	int n = 0, k = 0;
+	std::cin >> n >> k;
+
+	std::vector<int> numbers(n);
+	for (int i = 0; i < n; i++) {
+		std::cin >> numbers[i];
+	}
+
+	std::cout << getKStatistics(&numbers[0], n, k) << std::endl;
+}
