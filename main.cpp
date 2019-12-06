0e2e8570fe4825eb2fcb127d59971d8944ebdac7
Timur <55026550+SVizor42@users.noreply.github.com>
2019-12-06 11:42:32 +0300

    After review changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-﻿/*
-
+/*
 8. Хеш-таблица
-
 Реализуйте структуру данных типа “множество строк” на основе динамической хеш-таблицы с открытой адресацией. Хранимые строки непустые и состоят из строчных латинских букв. Хеш-функция строки должна быть реализована с помощью вычисления значения многочлена методом Горнера. Начальный размер таблицы должен быть равным 8-ми. Перехеширование выполняйте при добавлении элементов в случае, когда коэффициент заполнения таблицы достигает 3/4. Структура данных должна поддерживать операции добавления строки в множество, удаления строки из множества и проверки принадлежности данной строки множеству. 
 1_1. Для разрешения коллизий используйте квадратичное пробирование. i-ая проба g(k, i)=g(k, i-1) + i (mod m). m - степень двойки.
-
 */
 
 #include <assert.h>
@@ -14,39 +11,52 @@
 
 const size_t a = 31;
 
-size_t Hash(const std::string& key, size_t size) {
-    size_t hash = 0;
-    for (char sym: key) {
-        hash = (hash * a + sym) % size;
+struct Hash {
+    size_t operator()(const std::string& key, size_t size) {
+        size_t hash = 0;
+        for (char sym: key) {
+            hash = (hash * a + sym) % size;
+        }
+        return hash;
     }
-    return hash;
-}
+};
 
+template <class T, class THash>
 class HashTable {
 public:
-    explicit HashTable(size_t initial_size);
+    explicit HashTable(size_t initial_size, T init_empty, T init_deleted);
     HashTable(const HashTable&) = delete;
     HashTable(HashTable&&) = delete;
     HashTable& operator=(const HashTable&) = delete;
     HashTable& operator=(HashTable&&) = delete;
 
-    bool Has(const std::string& key) const;
-    bool Add(const std::string& key);
-    bool Remove(const std::string& key);
+    bool Has(const T& key) const;
+    bool Add(const T& key);
+    bool Remove(const T& key);
 
 private:
     void Rehash();
-    std::vector<std::string> table;
+    std::vector<T> table;
     size_t table_size = 0;
+    T empty;
+    T deleted;
 };
 
-HashTable::HashTable(size_t initial_size) : table(std::vector<std::string>(initial_size, "")) {}
+template <class T, class THash>
+HashTable<T, THash>::HashTable(size_t initial_size, T init_empty, T init_deleted) 
+    : empty(init_empty)
+    , deleted(init_deleted) 
+{
+    table = std::vector<T>(initial_size, empty);        
+}
 
-bool HashTable::Has(const std::string& key) const {
-    assert(!key.empty());
+template <class T, class THash>
+bool HashTable<T, THash>::Has(const T& key) const {
+    assert(key != empty);
 
-    size_t hash = Hash(key, table.size()), iter = 0;
-    while (table[hash] != "" && table[hash] != "Deleted") {
+    THash hash_func;
+    size_t hash = hash_func(key, table.size()), iter = 0;
+    while (table[hash] != empty && table[hash] != deleted) {
         if (table[hash] == key)
             return true;
         
@@ -57,14 +67,16 @@ bool HashTable::Has(const std::string& key) const {
     return false;
 }
 
-bool HashTable::Add(const std::string& key) {
-    assert(!key.empty());
+template <class T, class THash>
+bool HashTable<T, THash>::Add(const T& key) {
+    assert(key != empty);
     
     if (4 * table_size >= 3 * table.size())
         Rehash();
 
-    size_t hash = Hash(key, table.size()), iter = 0;
-    while (table[hash] != "") {
+    THash hash_func;
+    size_t hash = hash_func(key, table.size()), iter = 0;
+    while (table[hash] != empty && table[hash] != deleted) {
         if (table[hash] == key)
             return false;
             
@@ -77,13 +89,16 @@ bool HashTable::Add(const std::string& key) {
     return true;
 }
 
-bool HashTable::Remove(const std::string& key) {
-    assert(!key.empty());
+template <class T, class THash>
+bool HashTable<T, THash>::Remove(const T& key) {
+    assert(key != empty);
 
-    size_t hash = Hash(key, table.size()), iter = 0;
-    while (table[hash] != "") {
+    THash hash_func;
+    size_t hash = hash_func(key, table.size()), iter = 0;
+    while (table[hash] != empty) {
         if (table[hash] == key) {
-            table[hash] = "Deleted";
+            table[hash] = deleted;
+            table_size--;
             return true;
         }
         
@@ -94,11 +109,12 @@ bool HashTable::Remove(const std::string& key) {
     return false;
 }
 
-void HashTable::Rehash() {
-    HashTable new_table(2 * table.size());
+template <class T, class THash>
+void HashTable<T, THash>::Rehash() {
+    HashTable new_table(2 * table.size(), empty, deleted);
 
     for (size_t i = 0; i < table.size(); i++) {
-        if (table[i] != "" && table[i] != "Deleted")
+        if (table[i] != empty && table[i] != deleted)
             new_table.Add(table[i]);
     }
     
@@ -107,7 +123,7 @@ void HashTable::Rehash() {
 }
 
 int main() {
-    HashTable table(8);
+    HashTable<std::string, Hash> table(8, "", "Deleted");
     char command = ' ';
     std::string value;
     while (std::cin >> command >> value) {
@@ -125,5 +141,3 @@ int main() {
     }
     return 0;
 }
-
-
